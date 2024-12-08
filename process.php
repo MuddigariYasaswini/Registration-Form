576fdb83e1d8d145514dda1bcf18a3a254fb8506
MuddigariYasaswini <yasaswini.m2004@gmail.com>
2024-12-08 10:28:29 +0530

    Update process.php

--- process.php
+++ process.php
@@ -2,10 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $data = json_decode(file_get_contents('php://input'), true);
 
-  // Process form data
-  // ...
-
-  // Send success response
+  
   echo json_encode(['success' => true]);
 } else {
   // Handle invalid request
